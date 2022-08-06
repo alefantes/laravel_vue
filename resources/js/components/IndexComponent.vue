115a2bfa80113f7712033d857bbce605fbc8ea8d
xrynik7@gmail.com <Tel3454822>
2022-08-06 21:07:28 +0300

    vid16 Асинхронный CRUD

---
+++ resources/js/components/IndexComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="w-25">
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "IndexComponent",
+
+    data() {
+        return {
+        }
+
+    },
+
+    methods: {
+
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
