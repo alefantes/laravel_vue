1c14ce8594cf6b950a7236d53e11bc160b8b1318
xrynik7@gmail.com <Tel3454822>
2022-08-07 15:07:01 +0300

    vid17 Асинхронный CRUD

--- public/js/app.js
+++ public/js/app.js
@@ -5346,11 +5346,23 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   name: "IndexComponent",
   data: function data() {
     return {
-      people: null
+      people: null,
+      editPersonId: null
     };
   },
   mounted: function mounted() {
@@ -5364,6 +5376,12 @@ __webpack_require__.r(__webpack_exports__);
         console.log(res);
         _this.people = res.data;
       });
+    },
+    changeEditPersonId: function changeEditPersonId(id) {
+      this.editPersonId = id;
+    },
+    isEdit: function isEdit(id) {
+      return this.editPersonId === id;
     }
   }
 });
@@ -28476,18 +28494,70 @@ var render = function () {
       _vm._v(" "),
       _c(
         "tbody",
-        _vm._l(_vm.people, function (person) {
-          return _c("tr", [
-            _c("th", { attrs: { scope: "row" } }, [_vm._v(_vm._s(person.id))]),
-            _vm._v(" "),
-            _c("td", [_vm._v(_vm._s(person.name))]),
-            _vm._v(" "),
-            _c("td", [_vm._v(_vm._s(person.age))]),
-            _vm._v(" "),
-            _c("td", [_vm._v(_vm._s(person.job))]),
-          ])
-        }),
-        0
+        [
+          _vm._l(_vm.people, function (person) {
+            return [
+              _c("tr", [
+                _c("th", { attrs: { scope: "row" } }, [
+                  _vm._v(_vm._s(person.id)),
+                ]),
+                _vm._v(" "),
+                _c("td", [_vm._v(_vm._s(person.name))]),
+                _vm._v(" "),
+                _c("td", [_vm._v(_vm._s(person.age))]),
+                _vm._v(" "),
+                _c("td", [_vm._v(_vm._s(person.job))]),
+                _vm._v(" "),
+                _c("td", [
+                  _c(
+                    "a",
+                    {
+                      staticClass: "btn btn-success",
+                      attrs: { href: "#" },
+                      on: {
+                        click: function ($event) {
+                          $event.preventDefault()
+                          return _vm.changeEditPersonId(person.id)
+                        },
+                      },
+                    },
+                    [_vm._v("Edit")]
+                  ),
+                ]),
+              ]),
+              _vm._v(" "),
+              _c("tr", { class: _vm.isEdit(person.id) ? "" : "d-none" }, [
+                _c("th", { attrs: { scope: "row" } }, [
+                  _vm._v(_vm._s(person.id)),
+                ]),
+                _vm._v(" "),
+                _vm._m(1, true),
+                _vm._v(" "),
+                _vm._m(2, true),
+                _vm._v(" "),
+                _vm._m(3, true),
+                _vm._v(" "),
+                _c("td", [
+                  _c(
+                    "a",
+                    {
+                      staticClass: "btn btn-warning",
+                      attrs: { href: "#" },
+                      on: {
+                        click: function ($event) {
+                          $event.preventDefault()
+                          return _vm.changeEditPersonId(null)
+                        },
+                      },
+                    },
+                    [_vm._v("Update")]
+                  ),
+                ]),
+              ]),
+            ]
+          }),
+        ],
+        2
       ),
     ]),
   ])
@@ -28506,7 +28576,33 @@ var staticRenderFns = [
         _c("th", { attrs: { scope: "col" } }, [_vm._v("Age")]),
         _vm._v(" "),
         _c("th", { attrs: { scope: "col" } }, [_vm._v("Job")]),
+        _vm._v(" "),
+        _c("th", { attrs: { scope: "col" } }, [_vm._v("Edit")]),
       ]),
+    ])
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("td", [
+      _c("input", { staticClass: "form-control", attrs: { type: "text" } }),
+    ])
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("td", [
+      _c("input", { staticClass: "form-control", attrs: { type: "number" } }),
+    ])
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("td", [
+      _c("input", { staticClass: "form-control", attrs: { type: "text" } }),
     ])
   },
 ]
