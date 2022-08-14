2bc96b97c3a28160f5b14b81ff13cdc07fa43a91
xrynik7@gmail.com <Tel3454822>
2022-08-14 09:42:57 +0300

    vid33 vuex-install

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import store from "./store"; //импорт Vuex но импортируем всю папку сторе
 import router from "./router";
 import Index from "./components/Index";
 //[]-всегда для массива {}всегда для объекта
@@ -9,5 +10,6 @@ const app = new Vue({
     components :{
         Index
     },
-    router //подключаем просо роутер, так как он не компонент а JS файл
+    router, //подключаем просо роутер, так как он не компонент а JS файл
+    store
 }).$mount('#app')
